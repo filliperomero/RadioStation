6a45bb22e1de5942b880673a44f294b5ae96106b
FIllipe Romero <fillipe.romero@gmail.com>
2022-03-20 14:22:05 -0600

    chore: add controller tests

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -36,4 +36,66 @@ describe('#Controller - Test suite for controller calls', () => {
     expect(type).toStrictEqual(mockType)
   })
 
+  test('#createClientStream', async () => {
+    const mockStream = TestUtil.generateReadableStream(['test'])
+    const mockId = '1'
+    jest.spyOn(
+      Service.prototype,
+      Service.prototype.createClientStream.name,
+    ).mockReturnValue({ id: mockId, clientStream: mockStream })
+
+    jest.spyOn(Service.prototype, Service.prototype.removeClientStream.name).mockReturnValue()
+
+    const controller = new Controller()
+    const { stream, onClose } = controller.createClientStream()
+
+    onClose()
+
+    expect(stream).toStrictEqual(mockStream)
+    expect(Service.prototype.removeClientStream).toHaveBeenCalledWith(mockId)
+    expect(Service.prototype.createClientStream).toHaveBeenCalled()
+
+  })
+  describe('handleCommand', () => {
+    test('Command stop', async () => {
+      jest.spyOn(Service.prototype, Service.prototype.stopStreamming.name).mockResolvedValue()
+
+      const controller = new Controller()
+      const data = { command: ' stop ' }
+
+      const result = await controller.handleCommand(data)
+
+      expect(result).toStrictEqual({ result: 'ok' })
+      expect(Service.prototype.stopStreamming).toHaveBeenCalled()
+    })
+
+    test('Command start', async () => {
+      jest.spyOn(Service.prototype, Service.prototype.startStreamming.name).mockResolvedValue()
+
+      const controller = new Controller()
+      const data = { command: ' START ' }
+      
+      const result = await controller.handleCommand(data)
+
+      expect(result).toStrictEqual({ result: 'ok' })
+      expect(Service.prototype.startStreamming).toHaveBeenCalled()
+    })
+    
+    test('Command fxName', async () => {
+      const fxName = 'applause'
+      jest.spyOn(Service.prototype, Service.prototype.readFxByName.name).mockResolvedValue(fxName)
+
+      jest.spyOn(Service.prototype, Service.prototype.appendFxStream.name).mockReturnValue()
+
+      const controller = new Controller()
+      const data = { command: 'MY_FX_NAME' }
+
+      const result = await controller.handleCommand(data)
+
+      expect(result).toStrictEqual({ result: 'ok' })
+      expect(Service.prototype.readFxByName).toHaveBeenCalledWith(data.command.toLowerCase())
+      expect(Service.prototype.appendFxStream).toHaveBeenCalledWith(fxName)
+    })
+  })
+
 })
