080afa475fc3ba629b9118c6d2bb4f7dddbe128e
FIllipe Romero <fillipe.romero@gmail.com>
2022-03-20 14:19:40 -0600

    chore: add exception handler's to server

--- server/index.js
+++ server/index.js
@@ -5,3 +5,9 @@ import config from './config.js'
 
 server().listen(config.port)
   .on('listening', () => logger.info(`Server Running at ${config.port}`))
+
+// Make the application non stopable
+process.on('uncaughtException', 
+  (error) => logger.error(`UnhandledRejection happened: ${error.stack || error }`))
+process.on('unhandledRejection', 
+  (error) => logger.error(`UnhandledRejection happened: ${error.stack || error }`))
